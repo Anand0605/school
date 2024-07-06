f77f8e172a5553f6984dd7a3bf4903097b422731
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-07-06 23:54:12 +0530

    footer update

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar/index'
+import Footer from './Footer'
+
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div>
+      <Navbar />
+      <div>
+        <img src="./images/contact-us.svg" alt="" />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
